463e2dfaf5c37a383f0a36a7eae44972752e3873
benjamin <benjamin.carrasco@alumnos.ubiobio.cl>
2025-11-10 18:59:51 -0300

    Add ruta en el index

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,13 +3,14 @@ import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import parkingRoutes from "./parking.routes.js";
-
+import bicicletaRoutes from "./bicicleta.routes.js";
 const router = Router();
 
 router
     .use("/auth", authRoutes)
     .use("/user", userRoutes)
-    .use("/parking", parkingRoutes);
+    .use("/parking", parkingRoutes)
+    .use("/bicicleta", bicicletaRoutes);
 
 
 export default router;
